e434bb875b50d5c1d38ffc749165092fdb94ae35
Vipin Kumar <114799810+Its-Vipin@users.noreply.github.com>
2023-11-03 02:18:54 +0530

    Update mergeSort.c

--- mergeSort.c
+++ mergeSort.c
@@ -1,116 +1,112 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<pthread.h>
 
-#define ARRAY_SIZE 10
+int arr[10] = {99, 22, 00, 88, 11, 100, 33, 66, 44, 55};
 
-int numbers[ARRAY_SIZE];
-int sorted_numbers[ARRAY_SIZE];
 
-// Function to sort the first half of the array
-void* sortFirstHalf(void* arg) {
-    int* arr = (int*)arg;
-    int half = ARRAY_SIZE / 2;
+int arr_first_half[5], arr_second_half[5], final_arr[10];
 
-    for (int i = 0; i < half - 1; i++) {
-        for (int j = 0; j < half - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
 
-    pthread_exit(NULL);
+void *final_merge_sort(){
+
+	for(int i=0; i<5; i++){
+		final_arr[i] = arr_first_half[i];
+		final_arr[i+5] = arr_second_half[i];
+	}
+	
+	printf("# Merged array is: ");
+	for(int i=0; i<10; i++){
+		printf("%d ", final_arr[i]);
+	}
+	printf("\n");
+	
+	for(int i=0; i<10; i++){
+		for(int j=0; j<10; j++){
+			if(final_arr[i] < final_arr[j]){
+				int temp = final_arr[i];
+				final_arr[i] = final_arr[j];
+				final_arr[j] = temp;
+			}
+		}
+	}
+
+	printf("@ Final Merged & Sorted array is: ");
+	for(int i=0; i<10; i++){
+		printf("%d ", final_arr[i]);
+	}
+	printf("\n");
+	printf("\n");
+
+	pthread_exit(NULL);
 }
 
-// Function to sort the second half of the array
-void* sortSecondHalf(void* arg) {
-    int* arr = (int*)arg;
-    int half = ARRAY_SIZE / 2;
 
-    for (int i = half; i < ARRAY_SIZE - 1; i++) {
-        for (int j = half; j < ARRAY_SIZE - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    pthread_exit(NULL);
+void *individual_sort(){
+	for(int i=0; i<5; i++){
+		for(int j=0; j<5; j++){
+			if(arr_first_half[i] < arr_first_half[j]){
+				int temp = arr_first_half[i];
+				arr_first_half[i] = arr_first_half[j];
+				arr_first_half[j] = temp;
+			}
+			if(arr_second_half[i] < arr_second_half[j]){
+				int temp = arr_second_half[i];
+				arr_second_half[i] = arr_second_half[j];
+				arr_second_half[j] = temp;	
+			}
+		}
+	}
+		
+	pthread_exit(NULL);
 }
 
-// Function to merge and sort the two sorted halves
-void* mergeAndSort(void* arg) {
-    int half = ARRAY_SIZE / 2;
-    int* arr1 = numbers;
-    int* arr2 = numbers + half;
-    int* result = sorted_numbers;
 
-    int i = 0, j = half, k = 0;
+int main()
+{
+	printf("\n");		
+	
+	/* 
+	printf("Enter 10 elements in the array: ");
+	for(int i=0; i<10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("\n");
+	*/
+		
+	printf("# Initial input array is: ");
+	for(int i=0; i<10; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 
-    while (i < half && j < ARRAY_SIZE) {
-        if (arr1[i] < arr2[j]) {
-            result[k] = arr1[i];
-            i++;
-        } else {
-            result[k] = arr2[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < half) {
-        result[k] = arr1[i];
-        i++;
-        k++;
-    }
-
-    while (j < ARRAY_SIZE) {
-        result[k] = arr2[j];
-        j++;
-        k++;
-    }
-
-    pthread_exit(NULL);
-}
-
-int main() {
-    pthread_t threads[3];
-
-    // Initialize the array of numbers with random values for demonstration purposes
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        numbers[i] = rand() % 100;
-    }
-
-    // Create threads to sort the first and second halves
-    pthread_create(&threads[0], NULL, sortFirstHalf, numbers);
-    pthread_create(&threads[1], NULL, sortSecondHalf, numbers);
-
-    // Wait for the first two threads to complete
-    for (int i = 0; i < 2; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    // Create a thread to merge and sort the two sorted halves
-    pthread_create(&threads[2], NULL, mergeAndSort, NULL);
-
-    // Wait for the third thread to complete
-    pthread_join(threads[2], NULL);
-
-    // Print the sorted array
-    printf("Original array: ");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d ", numbers[i]);
-    }
-    printf("\nSorted array: ");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d ", sorted_numbers[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
+	
+	for(int i=0; i<5; i++){
+		arr_first_half[i] = arr[i];
+		arr_second_half[i] = arr[i+5];
+	}
+	
+	
+	
+	pthread_t parent_thread;
+	pthread_create(&parent_thread, NULL, individual_sort, NULL);
+	pthread_join(parent_thread, NULL);
+	
+	printf("* First half sorted array is: ");
+	for(int i=0; i<5; i++){
+		printf("%d ", arr_first_half[i]);
+	}
+	printf("\n");
+	
+	
+	printf("* Second half sorted array is: ");
+	for(int i=0; i<5; i++){
+		printf("%d ", arr_second_half[i]);
+	}
+	printf("\n");
+	
+	pthread_create(&parent_thread, NULL, final_merge_sort, NULL);
+	pthread_join(parent_thread, NULL);
+	
+	return 0;
+} 
